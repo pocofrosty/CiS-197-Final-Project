51b5bbf2cdfabb11e49860f833720a53b3fb70dd
pocofrosty <91432961+pocofrosty@users.noreply.github.com>
2022-04-11 13:36:58 -0400

    hex-grid-react
    Testing with hexgrid package + added favicon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 
+import {
+  GridGenerator, HexGrid, Layout, Path, Hexagon, Text, Pattern, Hex,
+} from 'react-hexgrid'
+
 function App() {
+  const hexagonSize = { x: 10, y: 10 }
+  const moreHexas = GridGenerator.parallelogram(-2, 2, -2, 2)
   return (
     <div>
-      Hello
+      <HexGrid>
+        <Layout size={hexagonSize} origin={{ x: 25, y: -30 }}>
+          {moreHexas.map((hex, i) => <Hexagon key={`${hex.q}${hex.r}${hex.s}`} q={hex.q} r={hex.r} s={hex.s} />)}
+        </Layout>
+      </HexGrid>
     </div>
   )
 }
