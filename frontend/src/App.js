75cce6b4766d28390234fe84d734a99c3ac0328c
pocofrosty <91432961+pocofrosty@users.noreply.github.com>
2022-04-15 15:07:47 -0400

    Hexagonal Board
    Hexagonal Board Implementation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,46 @@
 import React, { useState } from 'react'
 import axios from 'axios'
-// import {
-//   GridGenerator, HexGrid, Layout, HexUtils, Hexagon, Text,
-// } from 'react-hexgrid'
 import {
   Routes, Route, Outlet, Link,
 } from 'react-router-dom'
+
+import {
+  HexGrid, Layout, Hexagon, Text, Pattern, Path, Hex,
+} from 'react-hexgrid'
 
 import SignUpForm from './components/SignUpForm'
 import LoginForm from './components/LoginForm'
 
 function App() {
-  // const hexagonSize = { x: 10, y: 10 }
-  // const hexagonGenerator = GridGenerator.parallelogram(-2, 2, -2, 2)
-
-  // const generator = GridGenerator.getGenerator('hexagon')
-  // const hexagons = generator.apply(this, [3])
-
-  const [currentUsername, setCurrentUsername] = useState('')
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Hub />}>
-          <Route path="signup" element={<SignUpForm />} />
-          <Route path="login" element={<LoginForm setCurrentUsername={setCurrentUsername} />} />
-          <Route path="*" element={<Test />} />
-        </Route>
-      </Routes>
+    <div>
+      <label> test </label>
+      <HexGrid width={1200} height={800} viewBox="-50 -50 100 100">
+        {/* Grid with manually inserted hexagons */}
+        <Layout size={{ x: 10, y: 10 }} flat spacing={1.1} origin={{ x: 0, y: 0 }}>
+          <Hexagon q={0} r={0} s={0} />
+          {/* Using pattern (defined below) to fill the hexagon */}
+          <Hexagon q={0} r={-1} s={1} fill="pat-1" />
+          <Hexagon q={0} r={1} s={-1} />
+          <Hexagon q={1} r={-1} s={0}>
+            <Text>1, -1, 0</Text>
+          </Hexagon>
+          <Hexagon q={1} r={0} s={-1}>
+            <Text>1, 0, -1</Text>
+          </Hexagon>
+          {/* Pattern and text */}
+          <Hexagon q={-1} r={1} s={0} fill="pat-2">
+            <Text>-1, 1, 0</Text>
+          </Hexagon>
+          <Hexagon q={-1} r={0} s={1} />
+          <Hexagon q={-2} r={0} s={1} />
+          <Path start={new Hex(0, 0, 0)} end={new Hex(-2, 0, 1)} />
+        </Layout>
+        <Pattern id="pat-1" link="http://cat-picture" />
+        <Pattern id="pat-2" link="http://cat-picture2" />
+      </HexGrid>
     </div>
   )
 }
-
-const Hub = () => (
-  <div>
-    <Outlet />
-  </div>
-)
-
-const Test = () => (
-  <div>
-    <h1>Invalid Site </h1>
-  </div>
-)
 
 export default App
---
+++ frontend/src/App2.js
@@ -0,0 +1,78 @@
+import React, { useState, Component } from 'react'
+import axios from 'axios'
+import {
+  Routes, Route, Outlet, Link,
+} from 'react-router-dom'
+
+import {
+  Pattern, HexGrid, Layout, Hexagon, Text, GridGenerator, HexUtils,
+} from 'react-hexgrid'
+
+import configs from './configurations.json'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    const config = configs.hexagon
+    const generator = GridGenerator.getGenerator(config.map)
+    const hexagons = generator.apply(this, config.mapProps)
+    this.state = { hexagons, config }
+  }
+
+  changeType(event) {
+    const name = event.currentTarget.value
+    const config = configs[name]
+    const generator = GridGenerator.getGenerator(config.map)
+    const hexagons = generator.apply(this, config.mapProps)
+    this.setState({ hexagons, config })
+  }
+
+  render() {
+    const { hexagons, config } = this.state
+    const { layout } = config
+    const size = { x: layout.width, y: layout.height }
+    return (
+      <div className="App">
+        <h2>Select grid type and configuration from dropdown.</h2>
+        <div>
+          <strong>Template: </strong>
+          <select onChange={ev => this.changeType(ev)}>
+            {Object.keys(configs).map(type => (
+              <option name={type}>{type}</option>
+            ))}
+          </select>
+        </div>
+        <hr />
+        <HexGrid width={config.width} height={config.height}>
+          <Layout size={size} flat={layout.flat} spacing={layout.spacing} origin={config.origin}>
+            {
+              // note: key must be unique between re-renders.
+              // using config.mapProps+i makes a new key when the goal template chnages.
+              hexagons.map((hex, i) => (
+                // eslint-disable-next-line react/no-array-index-key
+                <Hexagon key={config.mapProps + i} q={hex.q} r={hex.r} s={hex.s} fill="temp-pattern">
+                  <Text>{HexUtils.getID(hex)}</Text>
+                </Hexagon>
+              ))
+            }
+          </Layout>
+          <Pattern id="temp-pattern" link="file:///C:/Users/dzhen/Desktop/test.png" />
+        </HexGrid>
+      </div>
+    )
+  }
+}
+
+const Hub = () => (
+  <div>
+    <Outlet />
+  </div>
+)
+
+const Test = () => (
+  <div>
+    <h1>Invalid Site </h1>
+  </div>
+)
+
+export default App
